5897a2e75ff6e25321e0d2768c451b5f2273d493
Samudra Perera <spperera@uwaterloo.ca>
2024-02-07 15:37:35 -0500

    Temp

--- src/routes/privateRoute.jsx
+++ src/routes/privateRoute.jsx
@@ -1,9 +1,13 @@
-const ProtectedRoute = ({ isAuthenticated = false, redirectPath = "/" }) => {
-
-  return (
-    <>
-    </>
-  );
+const ProtectedRoute = ({
+  isAuthenticated = false,
+  redirectPath = "/",
+  children,
+}) => {
+  if (isAuthenticated) {
+    return { children };
+  } else {
+    console.log("Not Authenticated");
+  }
 };
 
 export default ProtectedRoute;
