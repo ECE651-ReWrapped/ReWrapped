a9753204cdbaafd522773be0149ce2df6a9091d9
zohaib <zohaibansari100@gmail.com>
2024-02-15 18:34:20 -0500

    dashboard UI implementation

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React from 'react';
+import { Grid, Typography, Container } from "@mui/material";
+import SongList from '../components/songList';
 
-const Dashboard = () => {
-  return <div>Dashboard</div>;
-};
+// todo: temp data until backend is done
+
+const sampleSongData = [
+  {
+    "name": "Cut To The Feeling",
+    "artist": "Carly Rae Jepsen",
+    "album": "Cut To The Feeling",
+    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
+  },
+  {
+    "name": "Cut To The Feeling",
+    "artist": "Carly Rae Jepsen",
+    "album": "Cut To The Feeling",
+    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
+  },
+  {
+    "name": "Cut To The Feeling",
+    "artist": "Carly Rae Jepsen",
+    "album": "Cut To The Feeling",
+    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
+  },
+  {
+    "name": "Cut To The Feeling",
+    "artist": "Carly Rae Jepsen",
+    "album": "Cut To The Feeling",
+    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
+  },
+  {
+    "name": "Cut To The Feeling",
+    "artist": "Carly Rae Jepsen",
+    "album": "Cut To The Feeling",
+    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
+  }
+
+]
+
+function Dashboard() {
+  return (
+    <div>
+      <Typography ml={2} mt={2} variant="h1">
+        ReWrapped.
+      </Typography>
+      <Typography ml={3} mt={2} mb={5} variant="subtitle1">Listen with your friends and discover new music</Typography>
+
+      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <Grid container spacing={10} direction={'column'}>
+          <Grid item >
+              <Typography padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Top Songs from your friends</Typography>
+              <SongList musicData={sampleSongData} />
+          </Grid>
+          <Grid item >
+              <Typography  padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Based on your groups listening history</Typography>
+              <SongList musicData={sampleSongData} />
+              </Grid>
+        </Grid>
+      </Container>
+      {/* <div className="row my-5">
+        <h3> Analytics - Charts</h3>
+        <ul>
+          <li>Compare all friends listening time</li>
+          <li>Item B</li>
+          <li>Item C</li>
+        </ul>
+      </div> */}
+    </div>
+
+
+  );
+}
 
 export default Dashboard;
