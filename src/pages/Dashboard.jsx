b70a67a601dd8226eeb85d482262c472ee370ba3
zohaib <zohaibansari100@gmail.com>
2024-02-21 11:57:06 -0500

    add app bar

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Typography, Container } from "@mui/material";
+import ResponsiveAppBar from '../components/appBar';
 import SongList from '../components/songList';
 
 // todo: temp data until backend is done
@@ -41,21 +42,51 @@ const sampleSongData = [
 function Dashboard() {
   return (
     <div>
-      <Typography ml={2} mt={2} variant="h1">
-        ReWrapped.
-      </Typography>
-      <Typography ml={3} mt={2} mb={5} variant="subtitle1">Listen with your friends and discover new music</Typography>
+      <ResponsiveAppBar />
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        ml={10} 
+        mt={7}
+        display={"flex"}
+        justifyContent={"right"}
+        flexDirection={"column"}
+        alignItems={"right"}
+      >
+        <Typography
+          variant="h3"
+          style={{
+            fontWeight: "bold",
+            color: "#1DB954",
+            fontStyle: "italic",
+          }}
+        >
+          Spotify
+        </Typography>
+        <Typography
+          variant="h3"
+          style={{
+            fontWeight: "bold",
+            color: "#000000",
+            fontStyle: "italic",
+          }}
+        >
+          ReWrapped.
+        </Typography>
+      </Grid>
+      <Typography ml={10} mt={2} mb={5} variant="subtitle1" color='#61758A'>Listen with your friends and discover new music</Typography>
 
       <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
         <Grid container spacing={10} direction={'column'}>
           <Grid item >
-              <Typography padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Top Songs from your friends</Typography>
-              <SongList musicData={sampleSongData} />
+            <Typography padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Top Songs from your friends</Typography>
+            <SongList musicData={sampleSongData} />
           </Grid>
           <Grid item >
-              <Typography  padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Based on your groups listening history</Typography>
-              <SongList musicData={sampleSongData} />
-              </Grid>
+            <Typography padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Based on your groups listening history</Typography>
+            <SongList musicData={sampleSongData} />
+          </Grid>
         </Grid>
       </Container>
       {/* <div className="row my-5">
