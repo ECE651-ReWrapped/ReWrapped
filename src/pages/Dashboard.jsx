0034b433052dff2a2b3de9457d3681b22e4d03aa
zohaib <zohaibansari100@gmail.com>
2024-02-25 01:17:45 -0500

    use recc/recent api

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,44 +2,49 @@ import React from 'react';
 import { Grid, Typography, Container } from "@mui/material";
 import ResponsiveAppBar from '../components/appBar';
 import SongList from '../components/songList';
-
-// todo: temp data until backend is done
-
-const sampleSongData = [
-  {
-    "name": "Cut To The Feeling",
-    "artist": "Carly Rae Jepsen",
-    "album": "Cut To The Feeling",
-    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
-  },
-  {
-    "name": "Cut To The Feeling",
-    "artist": "Carly Rae Jepsen",
-    "album": "Cut To The Feeling",
-    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
-  },
-  {
-    "name": "Cut To The Feeling",
-    "artist": "Carly Rae Jepsen",
-    "album": "Cut To The Feeling",
-    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
-  },
-  {
-    "name": "Cut To The Feeling",
-    "artist": "Carly Rae Jepsen",
-    "album": "Cut To The Feeling",
-    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
-  },
-  {
-    "name": "Cut To The Feeling",
-    "artist": "Carly Rae Jepsen",
-    "album": "Cut To The Feeling",
-    "albumArt": "https://i.scdn.co/image/ab67616d0000b2737359994525d219f64872d3b1"
-  }
-
-]
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Dashboard() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const name = queryParams.get('displayName');
+
+  const [rpData, setData] = useState(null);
+  const [rcData, setRcData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/recently-played/' + name);
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchRcData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/recommended/' + name);
+        const jsonData = await response.json();
+        setRcData(jsonData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchRcData();
+  }, []);
+  if (rpData === null || rcData === null) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <ResponsiveAppBar />
@@ -47,7 +52,7 @@ function Dashboard() {
         item
         xs={12}
         sm={6}
-        ml={10} 
+        ml={10}
         mt={7}
         display={"flex"}
         justifyContent={"right"}
@@ -77,15 +82,15 @@ function Dashboard() {
       </Grid>
       <Typography ml={10} mt={2} mb={5} variant="subtitle1" color='#61758A'>Listen with your friends and discover new music</Typography>
 
-      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Grid container spacing={10} direction={'column'}>
           <Grid item >
             <Typography padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Top Songs from your friends</Typography>
-            <SongList musicData={sampleSongData} />
+            <SongList musicData={rpData} />
           </Grid>
           <Grid item >
             <Typography padding='5px' style={{ textAlign: 'center' }} variant="h5" gutterBottom>Based on your groups listening history</Typography>
-            <SongList musicData={sampleSongData} />
+            <SongList musicData={rcData} />
           </Grid>
         </Grid>
       </Container>
