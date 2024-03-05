153d704c31c4572f90c0220b412ebc7929ea6731
Samudra Perera <spperera@uwaterloo.ca>
2024-03-05 00:08:35 -0500

    Feat: Added debounced Search, for dynamically querying on text greater than 3 chars

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IconButton, TextField } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { debounce } from "../utility/debounce";
+import axios from "axios";
 
 const Searchbar = () => {
   const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const debouncedSearch = debounce(async (query) => {
+    console.log("Submitted");
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_LOCAL}/searchUser`,
+        {
+          query: query,
+        },
+        { withCredentials: true }
+      );
+
+      console.log(res);
+    } catch (err) {
+      console.error("Search Error", err);
+    }
+  }, 500);
+
+  //Rerennder only on query greater than 3
+  useEffect(() => {
+    if (query.length >= 3) {
+      debouncedSearch(query);
+    }
+  }, [query]);
 
   return (
     <>
-      <form>
+      <form onSubmit={(e) => e.preventDefault()}>
         <TextField
           id="search-bar"
-          onInput={(e) => setQuery(e.target.value)}
+          onChange={(e) => setQuery(e.target.value)}
           label="Find users"
           variant="outlined"
           placeholder="Search..."
