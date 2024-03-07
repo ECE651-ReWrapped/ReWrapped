a467584efb1348ef633d91fc142386f2de628da7
zohaib <zohaibansari100@gmail.com>
2024-03-06 22:23:58 -0500

    update TopSongCard

--- src/components/TopSongCard.js
+++ src/components/TopSongCard.js
@@ -1,36 +1,38 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
+import { CiCirclePlus } from "react-icons/ci";
+import IconButton from '@mui/material/IconButton';
 
 function TopSongCard({ mData }) {
-  return (
-    <div>
-    <ListItem>
-        <ListItemAvatar>
-            <Avatar alt="album art" src={mData.thumbnail} />
-        </ListItemAvatar>
-        <ListItemText
-            primary={mData.songName}
-            secondary={
-                <React.Fragment>
-                    <Typography
-                        sx={{ display: 'inline' }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                    >
-                        {mData.artist}
-                    </Typography>
-                    {" -  "} {mData.genres}
-                </React.Fragment>
-            }
-        />
-    </ListItem>
-</div>
-  );
+    const cardStyle = {
+        backgroundColor: '#F0F2F5',
+        borderRadius: '12px',
+        padding: '10px',
+        marginBottom: '20px',
+    };
+
+    return (
+        <div style={cardStyle}>
+            <ListItem
+                secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                        <CiCirclePlus />
+                    </IconButton>
+                }
+            >
+                <ListItemAvatar>
+                    <Avatar alt="album art" src={mData.thumbnail} />
+                </ListItemAvatar>
+                <ListItemText
+                    primary={mData.songName}
+                    secondary= {mData.artist+ " - " + mData.genres}
+                />
+            </ListItem>
+        </div>
+    );
 };
 
 export default TopSongCard;
