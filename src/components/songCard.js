83575c93edefaddf1c8a1a9047530e110e235f93
zohaib <zohaibansari100@gmail.com>
2024-03-08 16:33:59 -0500

    improve song card/ song list UI

--- src/components/songCard.js
+++ src/components/songCard.js
@@ -6,34 +6,61 @@ import ListItemText from '@mui/material/ListItemText';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import Avatar from '@mui/material/Avatar';
 
-function SongCard({ artist, album, name, albumArt }) {
-    return (
-
-        <div>
-            <ListItem>
-                <ListItemAvatar>
-                    <Avatar alt="album art" src={albumArt} />
-                </ListItemAvatar>
-                <ListItemText
-                    primary={name}
-                    secondary={
-                        <React.Fragment>
-                            <Typography
-                                sx={{ display: 'inline' }}
-                                component="span"
-                                variant="body2"
-                                color="text.primary"
-                            >
-                                {artist}
-                            </Typography>
-                            {" -  "} {album}
-                        </React.Fragment>
-                    }
-                />
-            </ListItem>
-            <Divider />
-        </div>
-    );
+function SongCard({ artist, album, name, albumArt, cIndex }) {
+    if(cIndex !== 6) {
+        return (
+            <div>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="album art" src={albumArt} />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={name}
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    {artist}
+                                </Typography>
+                                {" -  "} {album}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItem>
+                <Divider />
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <ListItem>
+                    <ListItemAvatar>
+                        <Avatar alt="album art" src={albumArt} />
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={name}
+                        secondary={
+                            <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    {artist}
+                                </Typography>
+                                {" -  "} {album}
+                            </React.Fragment>
+                        }
+                    />
+                </ListItem>
+            </div>
+        );
+    }
 }
 
 export default SongCard;
--- src/components/songList.js
+++ src/components/songList.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import SongCard from './songCard';
 import { List, Container } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Chip from '@mui/material/Chip';
 
 function SongList({ musicData }) {
     return (
-        <Container maxWidth="sm" style={{ borderRadius: '10px', border: '2px solid #ccc'}}>
-            <List>
-                {musicData.slice(0,10).map((music, index) => (
-                    <SongCard
-                        name={music.track_name}
-                        artist={music.artists}
-                        album={""}
-                        albumArt={''}
-                        // album={music.album}
-                        // albumArt={music.albumArt}
-                    />
-                ))}
-            </List>
-        </Container>
+        <TableContainer style={{ borderRadius: '10px', border: '1px solid #ccc' }}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Track</TableCell>
+                        <TableCell >Artist</TableCell>
+                        <TableCell align="right">Genres</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {musicData.map((row) => (
+                        <TableRow
+                            key={row.track_name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.track_name}
+                            </TableCell>
+                            <TableCell>{row.artists}</TableCell>
+                            <TableCell align="right"><Chip label="Alt Rock, Metal" /></TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+     
+      
     );
 }
 
