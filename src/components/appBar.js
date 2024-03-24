915b358f8d460a63248579d358e2516d8516ee59
Prerona <preronaghosh1997@gmail.com>
2024-03-24 18:24:32 -0400

    fixing routing on navbar logo

--- src/components/appBar.js
+++ src/components/appBar.js
@@ -172,7 +172,7 @@ function ResponsiveAppBar() {
             variant="h5"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="/dashboard"
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
