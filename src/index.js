ad44d677afd8e923a235b09242835f4552bed5ff
Prerona <preronaghosh1997@gmail.com>
2024-03-25 14:10:39 -0400

    removing strict mode

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ThemeProvider>
 );
