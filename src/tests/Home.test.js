d0d6052a86ce38a84d29cec4737f11c15e9f4cbf
Samudra Perera <spperera@uwaterloo.ca>
2024-04-01 11:42:03 -0400

    Home page tests

--- src/tests/Home.test.js
+++ src/tests/Home.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../containers/Login";
+import Signup from "../containers/Signup";
+import { createTestStore } from "../store/TestStore";
+import { Provider } from "react-redux";
 
 describe("Home Component Tests", () => {
   //Redner the component within the BrowswerRouter to support the Outlet component
@@ -23,5 +27,41 @@ describe("Home Component Tests", () => {
     const spotifyText = screen.getByText("Spotify");
     expect(spotifyText).toHaveStyle("color: #1DB954");
     expect(spotifyText).toHaveStyle("fontStyle: italic");
+  });
+});
+
+//Dealing with the Outlets
+const AppRoutes = () => (
+  <Routes>
+    <Route path="/" element={<Home />}>
+      <Route index element={<Login />} />
+      <Route path="signup" element={<Signup />} />
+    </Route>
+  </Routes>
+);
+
+describe("App routing tests", () => {
+  test("renders Login component for the base route", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <AppRoutes />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText(/Sign in/i)).toBeInTheDocument();
+  });
+
+  test("render singup component for /signup", () => {
+    const testStore = createTestStore();
+    render(
+      <Provider store={testStore}>
+        <MemoryRouter initialEntries={["/signup"]}>
+          <AppRoutes />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      screen.getByRole("button", { name: /Sign up/i })
+    ).toBeInTheDocument();
   });
 });
