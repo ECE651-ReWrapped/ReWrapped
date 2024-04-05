a9c6cf21c7e5b0c64f8f5183429152a1cd0ba9a6
Samudra Perera <spperera@uwaterloo.ca>
2024-04-05 01:00:23 -0400

    Added further tests

--- src/tests/Appbar.test.js
+++ src/tests/Appbar.test.js
@@ -1,20 +1,47 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter for Link component
-import AppBar from '../components/appBar'; // Replace 'AppBar' with the actual name of your component
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import ResponsiveAppBar from "../components/appBar"; // Replace 'AppBar' with the actual name of your component
 
-describe('AppBar component', () => {
-    it('renders correctly', () => {
-        render(
-            <Router>
-                <AppBar />
-            </Router>
-        );
+describe("AppBar component", () => {
+  it("renders correctly", () => {
+    render(
+      <Router>
+        <ResponsiveAppBar />
+      </Router>
+    );
 
-        // Check if certain elements are rendered
-        expect(screen.getByText('S.R')).toBeInTheDocument(); // Check for the 'SR' text
-        expect(screen.getByLabelText('account of current user')).toBeInTheDocument(); // Check for the user account icon
-    });
+    // Check if certain elements are rendered
+    expect(screen.getByText("S.R")).toBeInTheDocument(); // Check for the 'SR' text
+    expect(
+      screen.getByLabelText("account of current user")
+    ).toBeInTheDocument(); // Check for the user account icon
+  });
 
-    // You can add more specific tests for interactions or navigation if needed
+  it("opens the navigation menu when the menu icon is clicked", () => {
+    render(
+      <Router>
+        <ResponsiveAppBar />
+      </Router>
+    );
+
+    // Assuming MenuIcon opens a menu with text 'Create Playlist'
+    fireEvent.click(screen.getByLabelText("account of current user"));
+    expect(screen.getByText("Profile")).toBeInTheDocument(); // Verifying the menu contains 'Profile' option
+  });
+
+  it("opens the user menu when the avatar is clicked", () => {
+    render(
+      <Router>
+        <ResponsiveAppBar />
+      </Router>
+    );
+
+    // Simulate clicking the user avatar to open the user menu
+    fireEvent.click(screen.getByLabelText("account of current user"));
+    // Check for one of the user menu items to verify the menu opened
+    expect(screen.getByText("Profile")).toBeInTheDocument();
+  });
+
+  // You can add more specific tests for interactions or navigation if needed
 });
