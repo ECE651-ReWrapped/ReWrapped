44446198803c29746ff1cd72ff2133b9aa110104
Karthik Prakash Sivakumar <karthiksiva1997@gmail.com>
2024-04-05 13:17:46 -0400

    fix: added changes

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
 
       if (res.status === 200) {
         sessionStorage.setItem('currentUserEmail', values.email); // to make it persist across browser refresh 
-        window.location.href = 'http://localhost:6001/loginSpotify';
+        window.location.href = 'https://rewrapped-be-production.up.railway.app/loginSpotify';
       } else {
         // Handle non-200 HTTP status codes if needed
         console.error("Signup failed with status:", res.status);
